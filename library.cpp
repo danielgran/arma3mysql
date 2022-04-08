694277772796d015294e1640e4969ef80ce2bb40
Daniel Gran <contact@grandaniel.com>
2022-04-08 16:16:00 +0200

    Changes and build to so file

--- library.cpp
+++ library.cpp
@@ -3,29 +3,15 @@
 #include "jdbc/cppconn/statement.h"
 #include <iostream>
 
+extern "C"
+{
+void RVExtension(char *output, int outputSize, const char *function);
+void RVExtensionVersion(char *output, int outputSize);
+int RVExtensionArgs(char *output, int outputSize, const char *function, const char **argv, int argc);
+};
+
 using namespace std;
 
-int main() {
-
-    sql::Driver *driver = get_driver_instance();
-
-    sql::Connection *connection = driver->connect("localhost", "root", "");
-
-    connection->setSchema("arma3");
-
-    sql::Statement *stmt = connection->createStatement();
-    sql::ResultSet *res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
-    while (res->next()) {
-        cout << "\t... MySQL replies: ";
-        /* Access column data by alias or column name */
-        cout << res->getString("_message") << endl;
-        cout << "\t... MySQL says it again: ";
-        /* Access column data by numeric offset, 1 is the first column */
-        cout << res->getString(1) << endl;
-    }
-
-
-    connection->nativeSQL("SHOW PROCESSLIST");
-
-
+void RVExtension(char *output, int outputSize, const char *function) {
+  strcpy(output, "Hello World.");
 }
