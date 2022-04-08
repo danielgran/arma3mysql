0b70d4b1b727b360d5961aa24bbf2bc937de4aaa
Daniel Gran <contact@grandaniel.com>
2022-04-08 16:59:29 +0200

    Removed folder

--- cmake-build-debug/test-behaviour/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/danielgran/Projects/arma3mysql/test-behaviour
-# Build directory: /Users/danielgran/Projects/arma3mysql/cmake-build-debug/test-behaviour
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/danielgran/Projects/arma3mysql/cmake-build-debug/test-behaviour/hello_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
