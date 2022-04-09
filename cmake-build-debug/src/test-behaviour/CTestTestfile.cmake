56b0f4588e7bae50b31bd6fb8f159b9f82694452
Daniel Gran <contact@grandaniel.com>
2022-04-09 12:20:28 +0200

    fix

--- cmake-build-debug/src/test-behaviour/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/danielgran/Projects/arma3mysql/src/test-behaviour
-# Build directory: /Users/danielgran/Projects/arma3mysql/cmake-build-debug/src/test-behaviour
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/danielgran/Projects/arma3mysql/cmake-build-debug/src/test-behaviour/hello_test[1]_include.cmake")
-subdirs("../../_deps/googletest-build")
