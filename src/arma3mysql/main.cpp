50a934073c2c9a19953f57743592f395942a53a9
Daniel Gran <contact@grandaniel.com>
2022-04-16 05:09:32 -0700

    removed constructors

--- src/arma3mysql/main.cpp
+++ src/arma3mysql/main.cpp
@@ -17,17 +17,11 @@ int RVExtensionArgs(char *output, int outputSize, const char *function, const ch
 static int alreadyLaunched = 0;
 static Extension *Main;
 
-static void __attribute__((constructor))
-extension_init(void)
-{
-
-  Dl_info dl_info;
-  dladdr((void*)extension_init, &dl_info);
-  Main = new Extension();
-  alreadyLaunched = 1;
-}
-
 void RVExtension(char *output, int outputSize, const char *function) {
+  if (!alreadyLaunched) {
+    Main = new Extension();
+    alreadyLaunched = 1;
+  }
   string returnValue;
   returnValue = Main->ProcessCommand((string) function);
   strcpy(output, returnValue.c_str());
@@ -37,11 +31,4 @@ void RVExtensionVersion(char *output, int outputSize) {
   string returnValue;
   returnValue = Extension::GetVersion();
   strcpy(output, returnValue.c_str());
-}
-
-
-int main() {
-  char *return_value = (char *) malloc(10000);
-  RVExtension(return_value, 20000, "version");
-  printf("%s", return_value);
 }
